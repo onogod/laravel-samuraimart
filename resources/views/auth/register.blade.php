8342f20f96a70dfedf420b054c23755592da234c
onogod <onogod@icloud.com>
2024-04-16 20:42:40 +0900

    0416.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,22 +1,22 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
+<!--<x-guest-layout>
+ {{--   <form method="POST" action="{{ route('register') }}"> --}}
         @csrf
 
-        <!-- Name -->
+         Name 
         <div>
             <x-input-label for="name" :value="__('Name')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
-        <!-- Email Address -->
+         Email Address 
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <!-- Password -->
+         Password
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
@@ -28,7 +28,7 @@
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <!-- Confirm Password -->
+         Confirm Password 
         <div class="mt-4">
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
@@ -41,12 +41,60 @@
 
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
+            {{--    {{ __('Already registered?') }} --}}
             </a>
 
             <x-primary-button class="ms-4">
-                {{ __('Register') }}
+             {{--   {{ __('Register') }} --}}
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> -->
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md mb-3">
+            <h3 class="col-md-5">新規会員登録</h3>
+
+            <hr>
+            <form method="POST" action="{{ route('register') }}">
+            @csrf
+
+            <div class="form-group row">
+                <label for="name" class="col-md-5 col-form-label text-md-left">氏名<span class="ml-1 samuraimart-require-input-label"><span class="samuraimart-require-input-label-text">必須</span></span></label>
+                <div class="col-md-7">
+                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror samuraimart-login-input" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="侍　太郎">
+
+                    @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>氏名を入力してください</strong>
+                    </span>
+                    @enderror
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="email" class="col-md-5 col-form-label text-md-left">メールアドレス<span="ml-1 samuraimart-require-input-label"><span class="samuraimart-require-input-label-text">必須</span></span></label>
+            <div class="col-md-7">
+                <input id="email" type="email" class="form-control @error('email')is-invalid @enderror samuraimart-login-input" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="samurai@samurai.com">
+
+            @error('email')
+            <span class="invalid-feedback" role="alert">
+                <strong>メールアドレスを入力してください</strong>
+            </span>
+            @enderror
+            </div>
+            </div>
+            <div class="form-group row">
+                <label for="postal_code" class="col-md-5 col-form-label text-md-left">
+
+                <div class="col-md-7">
+                    <input type="text" class="form-control @error('postal_code') is-invalid @enderror samuraimart-login-input" name="post_code" required placeholder="150-0043">
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -34,5 +34,33 @@
         </tr>
         @endforeach
     </table>
+    @extends('layouts.app')
+
+    @section('content')
+    <div class="row">
+        <div class="col-9">
+            <div class="container mt-4">
+                @foreach($products as $product)
+                <div class="col-3">
+                    <a href="{{ route('products.show',$product) }}">
+                        <img src="{{ asset('img/dummy.png') }}" class="img-thumbnail">
+                    </a>
+                    <div class="row">
+                        <div class="col-12">
+                            <p class="samuraimart-product-label mt-2">
+                                {{ $product->name }}<br>
+                                <label>{{ $product->price }}</label>
+                            </p>
+                        </div>
+                    </div>
+                    </div>
+                    @endforeach
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
 </body>
+
+
 </html>
